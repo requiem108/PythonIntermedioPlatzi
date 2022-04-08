b72d6e840a2b38800d338df94d1d58a63a82b872
Requiem108 <mori8307@gmail.com>
2022-04-07 21:40:40 -0500

    Finaliza Curso intermedio

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 def readData():
     try:        
@@ -11,16 +12,41 @@ def readData():
 
 
 def run():
-    discovered_word = ''
-    #while 
-    inputWord= ['a','b','m','e']
     
-    #word=readData()
-    word = 'embarcacion'
-    #for lyri
-    espacios = [lyrics if lyrics in inputWord else '_'  for lyrics in word ] 
-    print("".join(espacios))
-    #print('a' in inputWord)
+    cls = lambda: os.system ("cls")
+    inputWords= []
+    word=readData()
+    
+    continuar = True
+    #inputWord = input("Ingresa una letra")
+    
+    while continuar: 
+       
+        print("ingresa una letra o la palabra")        
+        inputWord = input().lower()#normalize word
+
+        #when inputWord is not number and not exist in inputwords and enter just one word
+        if(inputWord.isalpha() and (inputWord not in inputWords) and len(inputWord) == 1):
+            #clean window       
+            cls()
+            inputWords.append(inputWord)
+            espacios = [lyrics if lyrics in inputWords else '_'  for lyrics in word ] 
+            print("".join(espacios))
+
+            if(word == "".join(espacios)):
+                print('!!Felicidades la palabra es:'+word)
+                continuar = False
+
+        elif (len(inputWord) > 1):
+            #when try guess word
+            if(inputWord == word):
+                print('!!Felicidades la palabra es:'+word)
+                continuar = False
+            else:
+                print('La palabra no es:'+inputWord)
+        else:
+            print('No ingresaste una palabra valida o ya esta repetida')
+        
 
 
 if __name__ == '__main__':
